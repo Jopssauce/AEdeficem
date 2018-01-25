8fc9b2130f9aa5f8cbea6c6985aac2fe685c0e2b
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-01-25 22:54:01 +0800

    Turn System

---
+++ CAPSTN1_V2.0/Assets/Scripts/TurnSystem/TurnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+	public static TurnManager instance = null;
+
+	public float currentTurns;
+	public bool isTurnEnded;
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy (this.gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void AdvanceTurn()
+	{
+		if (isTurnEnded == true) 
+		{
+			//Replenish Resources
+			//Event Timer goes down
+			//Check for solved/Unresolved
+			//Deduct/Add region quality to regions
+			//Check for collpasing Regions
+			//Update region special resource
+			//Random Events Pop-up
+			//Begin Next Turn
+			isTurnEnded = false;
+		}
+	}
+}
