517fa4260e0d24387925d715484346fa1f17d240
BENILDE\11517215 <johnjoseph.asuncion@benilde.edu.ph>
2018-01-25 14:38:50 +0800

    New Capstn

---
+++ CAPSTN1_V2.0/Assets/Scripts/RegionsSystem/RegionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionManager : MonoBehaviour {
+
+	public static RegionManager instance = null;
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;	
+		}
+		else if (instance != this) 
+		{
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+}
