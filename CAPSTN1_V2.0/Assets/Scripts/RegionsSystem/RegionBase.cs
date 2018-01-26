8721aaa9586fe021019b6aebb31ed2d81dd9cd6c
DESKTOP-COJMMHL\Kinsworth <michaelgerard.rivera@benilde.edu.ph>
2018-01-26 13:23:22 +0800

    Region System

---
+++ CAPSTN1_V2.0/Assets/Scripts/RegionsSystem/RegionBase.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionBase : MonoBehaviour
+{
+    public enum RegionSpeciality
+    {
+        BASIC,
+        WATER,
+        POWER,
+        FOOD
+    }
+
+    [System.Serializable]
+    public class Resources
+    {
+        public int Water;
+        public int Power;
+        public int Food;
+    }
+
+    public RegionSpeciality RegionType;
+    public int RegionQuality;
+    public Resources RegionResources;
+
+
+    void Awake()
+    {
+        AssignRegionType();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AssignRegionType()
+    {
+        int rand = Random.Range(0, 3);
+
+        if (rand == 0)
+        {
+            this.RegionType = RegionSpeciality.BASIC;
+        }
+        else if (rand == 1)
+        {
+            this.RegionType = RegionSpeciality.WATER;
+        }
+        else if (rand == 2)
+        {
+            this.RegionType = RegionSpeciality.POWER;
+        }
+        else if (rand == 3)
+        {
+            this.RegionType = RegionSpeciality.FOOD;
+        }
+
+        AdjustResourceByType();
+
+    }
+
+    void AdjustResourceByType()
+    {
+        if (this.RegionType == RegionSpeciality.BASIC)
+        {
+            this.RegionResources.Water = 1;
+            this.RegionResources.Power = 1;
+            this.RegionResources.Food = 1;
+        }
+        else if (this.RegionType == RegionSpeciality.WATER)
+        {
+            this.RegionResources.Water = 2;
+            this.RegionResources.Power = 1;
+            this.RegionResources.Food = 1;
+        }
+        else if (this.RegionType == RegionSpeciality.POWER)
+        {
+            this.RegionResources.Water = 1;
+            this.RegionResources.Power = 2;
+            this.RegionResources.Food = 1;
+        }
+        else if (this.RegionType == RegionSpeciality.FOOD)
+        {
+            this.RegionResources.Water = 1;
+            this.RegionResources.Power = 1;
+            this.RegionResources.Food = 2;
+        }
+    }
+}
--- CAPSTN1_V2.0/Assets/Scripts/RegionsSystem/RegionManager.cs
+++ CAPSTN1_V2.0/Assets/Scripts/RegionsSystem/RegionManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RegionManager : MonoBehaviour {
+public class RegionManager : MonoBehaviour
+{
+    public List<GameObject> Region;
 
-	public static RegionManager instance = null;
+    private GameObject[] RegionHolder;
+    private List<RegionBase.RegionSpeciality> TypeCheckList;
+
+    public static RegionManager instance = null;
 	void Awake()
 	{
 		if (instance == null) 
@@ -17,4 +22,57 @@ public class RegionManager : MonoBehaviour {
 		}
 		DontDestroyOnLoad (gameObject);
 	}
+
+    void Start()
+    {
+
+        RegionHolder = GameObject.FindGameObjectsWithTag("Region");
+
+        TypeCheckList = new List<RegionBase.RegionSpeciality>();
+
+        foreach (var item in RegionHolder)
+        {
+            Region.Add(item);
+        }
+
+        foreach (var item in Region)
+        {
+            if (!TypeCheckList.Contains(item.GetComponent<RegionBase>().RegionType))
+            {
+                TypeCheckList.Add(item.GetComponent<RegionBase>().RegionType);
+            }
+        }
+
+        if (TypeCheckList.Count < 4)
+        {
+            foreach (var item in Region)
+            {
+                if (TypeCheckList.Contains(item.GetComponent<RegionBase>().RegionType))
+                {
+                    while (true)
+                    {
+                        item.GetComponent<RegionBase>().AssignRegionType();
+
+                        if (TypeCheckList.Contains(item.GetComponent<RegionBase>().RegionType)) break;
+                    }
+
+                }
+
+                continue;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckRegionQuality()
+    {
+
+    }
+
 }
