44556c675131b0c11fa761d68a9bf44c622bb202
DESKTOP-COJMMHL\Kinsworth <michaelgerard.rivera@benilde.edu.ph>
2018-01-27 18:09:02 +0800

    Fixed Luigi's dialogue box pop-up and everything related -Rivera

---
+++ CAPSTN1_V2.0/Assets/Scripts/Event Details/EventReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventReader : MonoBehaviour
+{
+    public Button EventOrigin;
+
+    public Text EventText;
+    public Text EventTitle;
+
+    public Button Unresolve;
+    public Button Resolve;
+
+    // Use this for initialization
+    void Start()
+    {
+        Unresolve.onClick.AddListener(IgnoreEvent);
+        Resolve.onClick.AddListener(ResolveEvent);
+    }
+
+    public void ResolveEvent()
+    {
+        EventOrigin.GetComponent<EventPopUpBase>().ResolveOnEnd = true;
+        Debug.Log(EventOrigin.GetComponent<EventPopUpBase>().ResolveOnEnd);
+        Destroy(this.gameObject);
+    }
+
+    public void IgnoreEvent()
+    {
+        EventOrigin.GetComponent<EventPopUpBase>().ResolveOnEnd = false;
+        Debug.Log(EventOrigin.GetComponent<EventPopUpBase>().ResolveOnEnd);
+        Destroy(this.gameObject);
+    }
+}
---
+++ CAPSTN1_V2.0/Assets/Scripts/Events/EventPopUpBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventPopUpBase : MonoBehaviour
+{
+    public string EventTitle;
+    public int TurnsBeforeFail;
+    [TextArea]
+    public string EventDetails;
+    public bool ResolveOnEnd;
+    public Canvas EventCanvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.GetComponent<Button>().onClick.AddListener(Click);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void Click()
+    {
+        Canvas newButton = Instantiate(EventCanvas) as Canvas;
+        newButton.GetComponent<EventReader>().EventOrigin = this.GetComponent<Button>();
+
+        newButton.GetComponent<EventReader>().EventTitle.text = EventTitle;
+        newButton.GetComponent<EventReader>().EventText.text = EventDetails;
+    }
+}
