e610c597ec5f39b6b4e3892f73bbad0dbea1d7bc
DESKTOP-COJMMHL\Kinsworth <michaelgerard.rivera@benilde.edu.ph>
2018-01-29 20:46:28 +0800

    Added some basic RegionStats scritp

---
+++ CAPSTN1_V2.0/Assets/Scripts/RegionsSystem/RegionStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionStats : MonoBehaviour
+{
+    private RegionBase RegionRef;
+
+    void Start()
+    {
+        RegionRef = this.GetComponent<RegionBase>();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log(RegionRef.name);
+    }
+}
