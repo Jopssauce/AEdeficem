11ba4234090368d87539dd39018ec7854298232a
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-01-29 22:26:32 +0800

    Updated Turn Manager and Event Manager

--- CAPSTN1_V2.0/Assets/Scripts/TurnSystem/TurnManager.cs
+++ CAPSTN1_V2.0/Assets/Scripts/TurnSystem/TurnManager.cs
@@ -6,6 +6,7 @@ public class TurnManager : MonoBehaviour {
 	public static TurnManager instance = null;
 	public ResourceManager 	resManager;
 	public EventManager 	eventManager;
+	public RegionManager	regionManager;
 	public float 	currentTurn;
 	public int 		turns;
 	public bool 	isTurnEnded;
@@ -33,6 +34,10 @@ public class TurnManager : MonoBehaviour {
 		{
 			eventManager = EventManager.instance;
 		}
+		if (RegionManager.instance != null)
+		{
+			regionManager = RegionManager.instance;
+		}
 	}
 
 	public void AdvanceTurn()
@@ -45,12 +50,55 @@ public class TurnManager : MonoBehaviour {
 				resManager.ReplenishResource();
 			}
 			//Event Timer goes down
-
 			//Check for solved/Unresolved
-			//Deduct/Add region quality to regions
+			//Spawn Event if limit is not reached
+			if (eventManager != null)
+			{
+				if (eventManager.EventList != null)
+				{
+						
+					if (eventManager.EventList.Count != 10)
+					{
+						eventManager.SpawnEvent();
+					}
+					
+					foreach (var item in eventManager.EventList.ToArray())
+					{
+						EventPopUpBase eventPopUp = item.GetComponent<EventPopUpBase> ();
+
+						if (eventPopUp.ResolveOnEnd == true)
+						{
+							eventManager.EventList.Remove(item);
+						}
+						if (eventPopUp.ResolveOnEnd == false)
+						{
+							eventPopUp.TurnsBeforeFail -= 1;
+
+							if (eventPopUp.TurnsBeforeFail <= 0)
+							{
+								//Deduct quality to regions
+								if (regionManager != null)
+								{
+									
+								}
+								eventManager.EventList.Remove(item);
+							}
+						}
+						
+					}
+
+				}
+			}
+			
 			//Check for collpasing Regions
 			//Update region special resource
-			//Random Events Pop-up
+			if (regionManager != null)
+			{
+				foreach (var item in regionManager.RegionList)
+				{
+					//If region quality is low. Lower resource gain from region
+				}
+			}
 			//Begin Next Turn
 			currentTurn++;
 			isTurnEnded = false;
