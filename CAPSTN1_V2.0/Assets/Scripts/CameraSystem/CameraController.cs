e26c10d776a38459216c6ef557d6d1e5ea8b8091
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-02-11 18:02:44 +0800

    Added camera controls

---
+++ CAPSTN1_V2.0/Assets/Scripts/CameraSystem/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float speed;
+
+
+	void Update () 
+	{
+		//Debug.Log(Camera.main.WorldToViewportPoint(Input.mousePosition));
+		if (Input.GetAxisRaw("Horizontal") != 0)
+		{
+			transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+		}
+		if (Input.GetAxisRaw("Vertical") != 0)
+		{
+			transform.position += Vector3.forward * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+		}
+
+		if (Input.mousePosition.x > Screen.width - 100)
+		{
+			transform.position += Vector3.right * 1 * speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.x < 100)
+		{
+			transform.position += Vector3.right * -1 * speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.y > Screen.height - 100)
+		{
+			transform.position += Vector3.forward * 1 * speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.y < 50)
+		{
+			transform.position += Vector3.forward * -1 * speed * Time.deltaTime;
+		}
+		Debug.Log(Input.mousePosition);
+	}
+}
