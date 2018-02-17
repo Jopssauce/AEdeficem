39d5790eac2a17f811c112ea6d9d44c4d48f5f75
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-02-17 21:19:08 +0800

    Singleton applied for AQscript

--- CAPSTN1_V2.0/Assets/Scripts/Action Queue/AQscript.cs
+++ CAPSTN1_V2.0/Assets/Scripts/Action Queue/AQscript.cs
@@ -6,9 +6,24 @@ using UnityEngine.Events;
 
 public class AQscript : MonoBehaviour {
 
+	public static AQscript instance = null;
     public Button CloseButton;
     public GameObject eventOrigin;
     // Use this for initialization
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy (this.gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
     void Start() 
 	{
         CloseButton.onClick.AddListener(RemoveFromQueue);
