dd6de23bee4fa08af33da324a16e52bb147a4939
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-03-08 14:21:02 +0800

    added a script just to disable buttons but its shit code

---
+++ CAPSTN1_V2.0/Assets/Scripts/Action Queue/ButtonDisable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonDisable : MonoBehaviour {
+
+    public Button TheEndTurnButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    // Note: Shit code ito pero this is it for now
+    public void DisableButton()
+    {
+        TheEndTurnButton.interactable = false;
+    }
+}
