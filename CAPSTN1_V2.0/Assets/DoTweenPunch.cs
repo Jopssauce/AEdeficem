17b48cf7cd264ca163724aa091f10c0106edaef5
Evaristo <11530852@benilde.edu.ph>
2018-03-09 11:51:20 +0800

    Added new plugin(DOTWEEN) and edited the end turn indicator

---
+++ CAPSTN1_V2.0/Assets/DoTweenPunch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DoTweenPunch : MonoBehaviour {
+
+	public Button EndTurnButton;
+
+	public void OnClick()
+	{
+		EndTurnButton.transform.DOPunchScale (new Vector3 (-0.1f, -0.1f), 1.0f, 10, (float)0.5f);
+	}
+}
