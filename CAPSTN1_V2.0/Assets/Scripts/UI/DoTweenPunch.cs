2f85e0ca667301efa0a1fd566734b086fb0ffc92
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-03-11 08:52:49 +0800

    fixed the animation wherein it keeps giving an error message

--- CAPSTN1_V2.0/Assets/Scripts/UI/DoTweenPunch.cs
+++ CAPSTN1_V2.0/Assets/Scripts/UI/DoTweenPunch.cs
@@ -8,10 +8,25 @@ public class DoTweenPunch : MonoBehaviour {
 
 	public Button EndTurnButton;
     private float duration = 1.0f;
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
 
 	public void OnClick()
 	{
 		EndTurnButton.transform.DOPunchScale (new Vector3 (-0.1f, -0.1f), duration, 10, (float)0.5f);
 	}
+		
+	public void MouseOver()
+	{
+		anim.SetBool ("IsMouseHover", true);
+	}
 
+	public void MouseExit()
+	{
+		anim.SetBool ("IsMouseHover", false);
+	}
 }
