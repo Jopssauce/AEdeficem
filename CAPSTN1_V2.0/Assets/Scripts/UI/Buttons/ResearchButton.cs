0e5dbbda4d8abcd31b9fbc03d1943bf6837ebcaa
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-03-12 21:57:50 +0800

    Re arranged prefabs

---
+++ CAPSTN1_V2.0/Assets/Scripts/UI/Buttons/ResearchButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchButton : MonoBehaviour 
+{
+	public int tierNum;
+	public ResearchPanel researchPanel;
+	public ResearchManager.ResearchTypes type;
+
+	public void Select()
+	{
+		researchPanel.SelectResearch(this);
+	}
+
+
+}
