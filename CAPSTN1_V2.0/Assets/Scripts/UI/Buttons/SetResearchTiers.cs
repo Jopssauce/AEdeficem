5f1437188f2345aa1630575fcf25b78621263b88
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-03-15 22:52:40 +0800

    Added button to research all technology

---
+++ CAPSTN1_V2.0/Assets/Scripts/UI/Buttons/SetResearchTiers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResearchTiers : MonoBehaviour {
+
+	ResearchManager researchManager;
+	void Start()
+	{
+		if (ResearchManager.instance != null)
+		{
+			researchManager = ResearchManager.instance;
+		}
+	}
+	public void Click()
+	{
+		researchManager.tierProgress.disasterPrepTier = 4;
+		researchManager.tierProgress.resourceProdTier = 4;
+		researchManager.tierProgress.transportEffTier = 4;
+		researchManager.tierProgress.regionalPlanTier = 4;
+	}
+}
