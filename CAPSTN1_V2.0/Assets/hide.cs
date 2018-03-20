839d436ecc8d04d5176ffa86ca1614135757a26c
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-03-20 20:23:48 +0800

    added a new script for panels that only need hiding

---
+++ CAPSTN1_V2.0/Assets/hide.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class hide : MonoBehaviour, IPointerDownHandler
+{
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		this.transform.parent.gameObject.SetActive (false);
+	}
+}
