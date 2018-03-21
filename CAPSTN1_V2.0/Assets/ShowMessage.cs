96647a359d374678d80db6a692934ab95ee2d106
Evaristo <11530852@benilde.edu.ph>
2018-03-21 13:36:02 +0800

    button info tooltip in city stats panel implemented

---
+++ CAPSTN1_V2.0/Assets/ShowMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMessage : MonoBehaviour {
+
+	public GameObject MessagePanel;
+	public void ShowMessagePanel()
+	{
+		MessagePanel.SetActive(true);
+	}
+}
