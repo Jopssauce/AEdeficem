c371bd5489133876eb576241c9b6c4d881f438ce
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-03-31 14:30:29 +0800

    modified the audiomanager to seperate music and sfx for better management in user adjusted volume

--- CAPSTN1_V2.0/Assets/Scripts/MenuOption.cs
+++ CAPSTN1_V2.0/Assets/Scripts/MenuOption.cs
@@ -9,21 +9,37 @@ public class MenuOption : MonoBehaviour {
 
 	void Start()
 	{
+		/*
 		VolumeSliders[0].value = FindObjectOfType<AudioManager> ().Sounds [3].Source.volume;
 		VolumeSliders[1].value = FindObjectOfType<AudioManager> ().Sounds [0].Source.volume;
 		VolumeSliders[1].value = FindObjectOfType<AudioManager> ().Sounds [1].Source.volume;
 		VolumeSliders[1].value = FindObjectOfType<AudioManager> ().Sounds [2].Source.volume;
+		*/
+
+		foreach (SoundFX s in FindObjectOfType<AudioManager>().Music)
+		{
+			VolumeSliders [0].value = s.Source.volume;
+		}
+
+		foreach (SoundFX s in FindObjectOfType<AudioManager>().Sounds)
+		{
+			VolumeSliders [1].value = s.Source.volume;
+		}
 	}
 
 	public void SetMusicVolume()
 	{
-		FindObjectOfType<AudioManager> ().Sounds [3].Source.volume = VolumeSliders[0].value;
+		foreach (SoundFX s in FindObjectOfType<AudioManager>().Music)
+		{
+			s.Source.volume = VolumeSliders [0].value;
+		}
 	}
 
 	public void SetSFXVolume()
 	{
-		FindObjectOfType<AudioManager> ().Sounds [0].Source.volume = VolumeSliders[1].value;
-		FindObjectOfType<AudioManager> ().Sounds [1].Source.volume = VolumeSliders[1].value;
-		FindObjectOfType<AudioManager> ().Sounds [2].Source.volume = VolumeSliders[1].value;
+		foreach (SoundFX s in FindObjectOfType<AudioManager>().Sounds)
+		{
+			s.Source.volume = VolumeSliders [1].value;
+		}
 	}
 }
--- CAPSTN1_V2.0/Assets/Scripts/Sounds/AudioManager.cs
+++ CAPSTN1_V2.0/Assets/Scripts/Sounds/AudioManager.cs
@@ -7,10 +7,20 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour {
 
 	public SoundFX[] Sounds;
+	public SoundFX[] Music;
 	// Use this for initialization
 	void Awake () 
 	{
 		foreach (SoundFX s in Sounds)
+		{
+			s.Source = gameObject.AddComponent<AudioSource> ();
+			s.Source.clip = s.SoundEffect;
+			s.Source.loop = s.Loop;
+			// this is to help jops not get heart attack
+			s.Source.volume = s.Volume;
+		}
+
+		foreach (SoundFX s in Music)
 		{
 			s.Source = gameObject.AddComponent<AudioSource> ();
 			s.Source.clip = s.SoundEffect;
@@ -24,12 +34,18 @@ public class AudioManager : MonoBehaviour {
 
 	void Start()
 	{
-		Play ("Theme");
+		PlayMusic ("Theme");
 	}
 	
 	public void Play (string name)
 	{
 		SoundFX s = Array.Find (Sounds, sound => sound.name == name);
+		s.Source.Play();
+	}
+		
+	public void PlayMusic(string name)
+	{
+		SoundFX s = Array.Find (Music, sound => sound.name == name);
 		s.Source.Play();
 	}
 }
