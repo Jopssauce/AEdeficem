78630c080116cc9d86a5701d4e69a85f4f9e0743
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-04-03 16:58:18 +0800

    added a test video for debug purposes :D also added cutscene

--- CAPSTN1_V2.0/Assets/MoviePlay.cs
+++ CAPSTN1_V2.0/Assets/MoviePlay.cs
@@ -2,12 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class MoviePlay : MonoBehaviour {
 
+	private VideoPlayer videoPlayer;
+	public GameObject loading;
+	public GameObject UIDisable;
 	// Use this for initialization
+
+	void Awake()
+	{
+		videoPlayer = GetComponent<VideoPlayer> ();
+	}
+
 	void Start () 
 	{
 		FindObjectOfType<AudioManager> ().StopMusic ("Theme");
+		videoPlayer.loopPointReached += OnEndVideo;
+	}
+
+	void OnEndVideo(VideoPlayer vp)
+	{
+		videoPlayer.Stop ();
+		loading.SetActive (true);
+		UIDisable.SetActive (false);
+		SceneManager.LoadSceneAsync("Main Scene");
 	}
 }
