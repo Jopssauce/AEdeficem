cb7145b2a42824f1a00e3fa3f820b82d0c01ea29
Luis Gabriel D. Evaristo <luisgabriel.evaristo@benilde.edu.ph>
2018-04-04 21:10:46 +0800

    fixed research panel where you just click the tab and the tumbnail will automatically be enabled

--- CAPSTN1_V2.0/Assets/Scripts/UI/ResearchTabs.cs
+++ CAPSTN1_V2.0/Assets/Scripts/UI/ResearchTabs.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ResearchTabs : MonoBehaviour, IPointerClickHandler {
 
+	public RawImage ThePanel;
+	public Texture Thumnails;
+
+	void Start()
+	{
+		ShowTab ();
+	}
+
     public void ShowTab()
     {
+		ThePanel.texture = Thumnails;
 		this.transform.parent.SetSiblingIndex (10);
     }
 
