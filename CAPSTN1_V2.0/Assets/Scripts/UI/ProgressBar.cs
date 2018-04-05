3801395b98a78519d424668a99af6ea118228016
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-04-05 09:04:54 +0800

    Added progress bar

---
+++ CAPSTN1_V2.0/Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+
+	public Slider slider;
+	public Text progressText;
+	
+	RegionManager regionManager;
+	TurnManager turnManager;
+	// Use this for initialization
+	void Start () 
+	{
+		if (RegionManager.instance != null)
+		{
+			regionManager = RegionManager.instance;
+		}
+		if (TurnManager.instance != null)
+		{
+			turnManager = TurnManager.instance;
+		}
+		turnManager.EndTurnEvent.AddListener(UpdateProgress);
+	}
+	
+	
+	void UpdateProgress()
+	{
+		float rqSum = 0;
+		foreach (var item in regionManager.regionList)
+		{
+			rqSum += item.regionQuality;
+		}
+		rqSum /= 500;
+		slider.value = rqSum;
+		float progressNum = rqSum * 100.0f;
+		progressText.text = progressNum.ToString() + "%";
+	}
+}
